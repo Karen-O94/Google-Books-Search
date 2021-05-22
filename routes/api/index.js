852a840c2e0e947d722adaab88b2d45fa2649ec7
Karen <karenyaa@live.co.uk>
2021-05-22 07:44:08 +0100

    changed header colour for cards

--- routes/api/index.js
+++ routes/api/index.js
@@ -10,9 +10,8 @@ router.use("/books", bookRoutes);
 router.use("/google", googleRoutes);
 
 // render the html page
-router.use(function(req, res) {
+router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-  });
-  
-  module.exports = router;
-  
+});
+
+module.exports = router;
