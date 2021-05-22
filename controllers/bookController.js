632cd3b7d9a5caabe76e0fe7fcb4767802adbcfc
Karen <karenyaa@live.co.uk>
2021-05-22 05:43:12 +0100

    completed find by id and create methods for books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,3 +6,14 @@ module.exports = {
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
+  findById: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+}
