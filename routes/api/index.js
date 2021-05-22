21d69a237e8c0113130a6a8ca4ced24a479ce4e3
Karen <karenyaa@live.co.uk>
2021-05-22 07:40:17 +0100

    added render html function in api-index.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,3 +8,11 @@ router.use("/books", bookRoutes);
 
 // Google Routes
 router.use("/google", googleRoutes);
+
+// render the html page
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  });
+  
+  module.exports = router;
+  
