06dfe6872546b4fbd56fa1eab079567c3f5fe63d
Karen <karenyaa@live.co.uk>
2021-05-22 12:18:45 +0100

    updating mongodb connection in server.js

--- server.js
+++ server.js
@@ -17,10 +17,12 @@ app.use(routes);
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/google_book_search",
+  process.env.MONGODB_URI || "mongodb://localhost/google_book_search",
   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
-    useNewUrlParser: true
+    useFindAndModify: false
   }
 );
 
