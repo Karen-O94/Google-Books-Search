602189b6c13d1a432a1b993f4e9ac1df9cada4f5
Karen <karenyaa@live.co.uk>
2021-05-22 06:20:37 +0100

    initial nav and footer component built

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="text-center">
+        <i className="fab fa-github" /> Built by Karen-O94
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
